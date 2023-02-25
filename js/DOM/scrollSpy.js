623c2817142c4007b81c7f2ca9e463b9c908ae4a
AxelSparta <axelnicolassparta@gmail.com>
2023-02-25 17:55:28 -0300

    photo added and project finished

--- js/DOM/scrollSpy.js
+++ js/DOM/scrollSpy.js
@@ -1,25 +1,25 @@
 const d = document
 
 export default function scrollSpy () {
-  const $sections = d.querySelectorAll('[data-scroll-spy]')
+  const $sections = d.querySelectorAll('[data-scroll-spy-section]')
   const observer = new IntersectionObserver(
     entries => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
           d.querySelector(
             `a[data-scroll-spy][href="#${entry.target.id}"]`
-          ).classList.add('nav-link-active')
+          ).classList.add('item__link--active')
         } else {
           d.querySelector(
             `a[data-scroll-spy][href="#${entry.target.id}"]`
-          ).classList.remove('nav-link-active')
+          ).classList.remove('item__link--active')
         }
       })
     },
     {
       //root por defecto es el document
       //rootMargin: "-300px", // zona interactiva de observación (al section lo empezará a observar con un margin de -300px)
-      threshold: 0.4
+      threshold: 0.6
     }
   )
 
--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 import { contactForm } from './DOM/contactForm.js'
 import hamburgerMenu from './DOM/hamburger_menu.js'
 import pageTheme from './DOM/pageTheme.js'
+import scrollSpy from './DOM/scrollSpy.js'
 const d = document
 
 d.addEventListener('DOMContentLoaded', e => {
   hamburgerMenu('#menu-btn', '#nav', '.item__link')
   pageTheme('.theme__button', '[data-theme]')
   contactForm()
+  scrollSpy()
 })
